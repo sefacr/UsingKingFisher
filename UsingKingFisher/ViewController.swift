7bf6f45aa072638ae05d24a4366bd0b5c3cf9276
sefacr <aacarsefa@gmail.com>
2024-05-22 17:43:01 +0300

    using kingfisher

--- UsingKingFisher/ViewController.swift
+++ UsingKingFisher/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageShow(imageName: "baklava")
+        
     }
-
-
+    
+    
+    @IBAction func image1Btn(_ sender: Any) {
+        imageShow(imageName: "su")
+    }
+    
+    @IBAction func image2Btn(_ sender: Any) {
+        imageShow(imageName: "lazanya")
+    }
+    
+    func imageShow(imageName: String){
+        if let url = URL(string: "//http://kasimadalan.pe.hu/yemekler/resimler/\(imageName).php") {
+            DispatchQueue.main.async {
+                self.imageView.kf.setImage(with: url)
+            }
+        }
+    }
+    
 }
 
